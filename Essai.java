753d6807215d3bae9d9b2b613716362e7b41adb1
AlmoustaphaDjibrilla <almoustaphadjibrillaidrissa@gmail.com>
2022-10-04 23:19:31 +0200

    Premier envoi sur internet

---
+++ Essai.java
@@ -0,0 +1,11 @@
+public class Essai{
+
+    public static void main (String[] args){
+
+        System.out.println("Bonjour à tous");
+
+        int i=0, j=5;
+        int somme= (int) (i+j);
+        System.out.println ("La somme de "+i+" et "+j+" est :" +somme);
+    }
+}
